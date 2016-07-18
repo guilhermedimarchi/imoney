7719c26d6059ff82e357124283e5c2d5645c9306
Eduardo <eduardo.duds27@gmail.com>
2016-07-18 19:45:10 -0300

    Adicionando Entidades do Core Data e separando o projeto em pasta de MVC

---
+++ iMoney/Moeda+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Moeda+CoreDataProperties.swift
+//  iMoney
+//
+//  Created by Student on 7/18/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Moeda {
+
+    @NSManaged var nome: String?
+
+}
